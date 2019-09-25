6c65350089686d0d87176dfa7403b798182878f5
macstudent <saitejasunkari04@gmail.com>
2019-09-25 19:36:02 -0400

    1

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,7 +1,42 @@
 package com.lambton;
 
-public class LambtonStringTools
-{
-    public void
+public class LambtonStringTools {
+
+public static String reverse(String s) {
+
+       StringBuilder st=new StringBuilder(s);
+        st.reverse();
+
+        return st.toString();
+}
+
+    public static int binaryToDecimal(int binary){
+        int decimal=0;
+        int p = 0;
+        while(true){
+            if(binary==0){
+                break;
+            }
+        else{
+                int temp = binary%10;
+                decimal += temp*Math.pow(2, p);
+                binary = binary/10;
+                p++;
+            }
+
+
+        }
+
+        return decimal;
+    }
+
+
+
+
+
+
+
+
+
 
 }
