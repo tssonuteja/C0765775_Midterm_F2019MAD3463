329122d4f8b713a02bfd454cb225f209a30d979c
macstudent <saitejasunkari04@gmail.com>
2019-09-25 19:36:34 -0400

    3

--- src/main/java/com/lambton/Test.java
+++ src/main/java/com/lambton/Test.java
@@ -1,4 +1,13 @@
 package com.lambton;
 
 public class Test {
+
+    public static void main(String[] args) {
+
+
+       System.out.println( LambtonStringTools.reverse("LAMBTON"));
+
+      System.out.println(LambtonStringTools.binaryToDecimal(1000));
+
+    }
 }
